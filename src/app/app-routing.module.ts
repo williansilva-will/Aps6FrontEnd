f3a9be6e4ee2bc1aa53f91618ff71ef09e5c5e3d
Willian K <willian.cardoso.silva.731@gmail.com>
2020-11-15 18:42:31 -0300

    adicionar_GuardaRota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthService } from './Servicos/auth.service';
 import { TelaCadastroComponent } from './Telas/tela-cadastro/tela-cadastro.component';
 import { TelaLoginComponent } from './Telas/tela-login/tela-login.component';
 import { TelaRegistrosComponent } from './Telas/tela-registros/tela-registros.component';
@@ -7,7 +8,7 @@ import { TelaRegistrosComponent } from './Telas/tela-registros/tela-registros.co
 const routes: Routes = [
   { path: 'ministerioAmbiente/login', component: TelaLoginComponent},
   { path: 'ministerioAmbiente/cadastro', component: TelaCadastroComponent},
-  { path: 'ministerioAmbiente/registros', component: TelaRegistrosComponent},
+  { path: 'ministerioAmbiente/registros', component: TelaRegistrosComponent, canActivate: [AuthService]},
   { path: '', component: TelaLoginComponent}
 ];
 
